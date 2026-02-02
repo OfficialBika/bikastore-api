28b73161cc9159fbddaaa6790b6a1b833dd8e4d3
Official Bika <phoetaay@gmail.com>
2026-02-03 00:03:07 +0700

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,18 @@
+// ------------------------------
+//  BIKA STORE — Review Model
+// ------------------------------
+
 import mongoose from "mongoose";
 
 const ReviewSchema = new mongoose.Schema(
   {
-    stars: { type: Number, min: 1, max: 5, required: true },
-    comment: String,
-    telegramUserId: Number,
-    telegramUsername: String,
+    userId: { type: Number, required: true },
+    username: { type: String },
+
+    rating: { type: Number, min: 1, max: 5, required: true },
+    reviewText: { type: String },
+
+    fromWeb: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
