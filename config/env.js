1e7949f4d484ea46fc1c63781fe4fb3afdc61eda
Official Bika <phoetaay@gmail.com>
2026-02-02 23:36:46 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 
+export const ENV = {
+  PORT: process.env.PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI,
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  BOT_API_URL: `https://api.telegram.org/bot${process.env.BOT_TOKEN}`,
+  BOT_ADMIN_ID: process.env.BOT_ADMIN_ID
+};
